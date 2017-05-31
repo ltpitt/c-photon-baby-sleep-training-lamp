6194ec3a723c64bdf8489b295d41094b0497ad09
Davide Nastri <d.nastri@gmail.com>
2017-05-31 20:17:31 +0200

    feat: Refactored setColor function
    Now setColor keeps the last color set and has a fade time parameter (in milliseconds)

--- c-photon-baby-sleep-training-lamp.ino
+++ c-photon-baby-sleep-training-lamp.ino
@@ -54,7 +54,12 @@ Led led(D0, D1, D2, false);
 Color red(255, 0, 0);
 Color green(0, 255, 0);
 Color blue(0, 0, 255);
-Color black(0, 0, 0);
+Color off(0, 0, 0);
+
+// Initializing all the previousIntensity variables to 0
+int previousIntensityRed = 0;
+int previousIntensityGreen = 0;
+int previousIntensityBlue = 0;
 
 // sendHttpRequest function
 void sendHttpRequest(String method, String hostname, int port, String path, String body) {
@@ -86,14 +91,16 @@ void sendHttpRequest(String method, String hostname, int port, String path, Stri
     }
 }
 
-bool setColor(String command) {
+int setColor(String command) {
     // Getting index of all the commas in the command
     int firstCommaIndex = command.indexOf(',');
     int secondCommaIndex = command.indexOf(',', firstCommaIndex+1);
+    int thirdCommaIndex = command.indexOf(',', secondCommaIndex+1);
     // Using that indexes to split the command subunits
     String intensityRed = command.substring(0, firstCommaIndex);
     String intensityGreen = command.substring(firstCommaIndex+1, secondCommaIndex);
     String intensityBlue = command.substring(secondCommaIndex+1);
+    String transitionMilliseconds = command.substring(thirdCommaIndex+1);
     // Printing to serial the color
     Serial.print("intensityRed: " + intensityRed);  
     Serial.println();
@@ -101,54 +108,18 @@ bool setColor(String command) {
     Serial.println();
     Serial.print("intensityBlue: " + intensityBlue);  
     Serial.println();
-    // Color variable
-    Color color(intensityRed.toInt(), intensityGreen.toInt(), intensityBlue.toInt());
-    // Setting the LED to the specified color
-    led.setColor(Color(color));
-    return true;
-}
-
-
-
-bool fadeColor(String command) {
-    // Getting index of all the commas in the command
-    int firstCommaIndex = command.indexOf(',');
-    int secondCommaIndex = command.indexOf(',', firstCommaIndex+1);
-    int thirdCommaIndex = command.indexOf(',', secondCommaIndex+1);
-    int fourthCommaIndex = command.indexOf(',', thirdCommaIndex+1);
-    int fifthCommaIndex = command.indexOf(',', fourthCommaIndex+1);
-    int sixthCommaIndex = command.indexOf(',', fifthCommaIndex+1);
-    // Using that indexes to split the command subunits
-    String intensityRedStart = command.substring(0, firstCommaIndex);
-    String intensityGreenStart = command.substring(firstCommaIndex+1, secondCommaIndex);
-    String intensityBlueStart = command.substring(secondCommaIndex+1, thirdCommaIndex);
-    String intensityRedEnd = command.substring(thirdCommaIndex+1, fourthCommaIndex);
-    String intensityGreenEnd = command.substring(fourthCommaIndex+1, fifthCommaIndex);
-    String intensityBlueEnd = command.substring(fifthCommaIndex+1, sixthCommaIndex);
-    String transitionMilliseconds = command.substring(sixthCommaIndex+1);
-    // Printing to serial the start color
-    Serial.print("intensityRedStart: " + intensityRedStart);  
-    Serial.println();
-    Serial.print("intensityGreenStart: " + intensityGreenStart);  
-    Serial.println();
-    Serial.print("intensityBlueStart: " + intensityBlueStart);  
-    Serial.println();
-    // Printing to serial the end color    
-    Serial.print("intensityRedEnd: " + intensityRedEnd);  
-    Serial.println();
-    Serial.print("intensityGreenEnd: " + intensityGreenEnd);  
-    Serial.println();
-    Serial.print("intensityBlueEnd: " + intensityBlueEnd);  
-    Serial.println();
     Serial.print("transitionMilliseconds: " + transitionMilliseconds);  
     Serial.println();
     // Start color variable
-    Color start(intensityRedStart.toInt(), intensityGreenStart.toInt(), intensityBlueStart.toInt());
+    Color start(previousIntensityRed, previousIntensityGreen, previousIntensityBlue);
     // End color variable
-    Color end(intensityRedEnd.toInt(), intensityGreenEnd.toInt(), intensityBlueEnd.toInt());
+    Color end(intensityRed.toInt(), intensityGreen.toInt(), intensityBlue.toInt());
     // Fade one from Start color to End color
     led.fadeOnce(start, end, transitionMilliseconds.toInt());
-    return true;
+    previousIntensityRed = intensityRed.toInt();
+    previousIntensityGreen = intensityGreen.toInt();
+    previousIntensityBlue = intensityBlue.toInt();
+    return 1;
 }
 
 bool buttonInputChecker() {
@@ -241,7 +212,7 @@ bool execute_CMD(byte CMD, byte Par1, byte Par2) {
     return true;
 }
 
-bool dfMini(String command) {
+int dfMini(String command) {
     // Getting index of all the commas in the command
     int firstCommaIndex = command.indexOf(',');
     int secondCommaIndex = command.indexOf(',', firstCommaIndex+1);
@@ -255,19 +226,37 @@ bool dfMini(String command) {
     Serial.println();
     // Execute the right function according to the one requested in the http call    
     if (function == "play") {
-        play();
+        if (play()) {
+            return 1;
+        } else {
+            return -1;
+        }
     } else if (function == "pause") {
-        pause();        
+        if (pause()) {
+            return 1;
+        } else {
+            return -1;
+        }
     } else if (function == "playPrevious") {
-        playPrevious();
+        if (playPrevious()) {
+            return 1;
+        } else {
+            return -1;
+        }
     } else if (function == "playNext") {
-        playNext();
+        if (playNext()) {
+            return 1;
+        } else {
+            return -1;
+        }
     } else if (function == "setVolume") {
-        setVolume(parameter.toInt());
+        if (setVolume(parameter.toInt())) {
+            return 1;
+        } else {
+            return -1;
+        }
     }
-    return true;
 }
-
 
 void setup() {
     // Starting serial port used for debugging
@@ -275,7 +264,6 @@ void setup() {
     // Starting serial port used to control Df Mini Mp3 Player
     Serial1.begin(9600);
     // Publish functions on the Particle Cloud
-    Particle.function("fadeColor", fadeColor);
     Particle.function("setColor", setColor);
     Particle.function("dfMini", dfMini);
     // When the Little Cloud is turned on the LED is off
@@ -295,8 +283,6 @@ void setup() {
 
 void loop() {
     buttonInputChecker();
-    // Pulse red from 0% brightness to 100% brightness every 5 seconds
-    //led.fade(red.withBrightness(10), red, 5000);
     // Example http calls
     //sendHttpRequest("post", "davidenastri.it", 8080, "/", "Ciao");
     //sendHttpRequest("get", "davidenastri.it", 8080, "/", "Ciao");
